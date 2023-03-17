eb15755fbe6f79560cf01edfca91f090129ba964
DESKTOP-5SC99UJ\philo <philomon@naver.com>
2023-03-17 11:03:54 +0900

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,30 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["*"],
+      methods: ["GET", "POST", "DELETE", "PATCH", "HEAD"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
